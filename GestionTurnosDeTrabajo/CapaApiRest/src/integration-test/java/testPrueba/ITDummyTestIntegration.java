f887b400bf530cbfcc5ece45692a68f1f37dab52
José Mansilla <jmansiya@gmail.com>
2016-02-26 20:39:33 +0100

    Se configura jacoco para la generación de informes de cobertura. Se configuran perfiles de maven para ejecución de test unitarios e integración. Se configura Spring-security para el logeo de la aplicación.

---
+++ GestionTurnosDeTrabajo/CapaApiRest/src/integration-test/java/testPrueba/ITDummyTestIntegration.java
@@ -0,0 +1,14 @@
+package testPrueba;
+import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class ITDummyTestIntegration {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
